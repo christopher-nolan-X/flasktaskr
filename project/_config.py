8c0f0b5803f5ee0ebc43bc730e414b674e13e231
Eric Koston Jr <EricKostonJr@gmail.com>
2020-06-24 20:06:48 -0400

    deployed commit

---
+++ project/_config.py
@@ -0,0 +1,18 @@
+import os
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = os.environ['SECRET_KEY']
+DEBUG = False
+
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
