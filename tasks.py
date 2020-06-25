cf95633d3cbb6a6a43ab12490c47c2529c69e81c
Eric Koston Jr <EricKostonJr@gmail.com>
2020-06-25 13:57:53 -0400

    migrate fabfile to invoke due to python3 incompatability

---
+++ tasks.py
@@ -0,0 +1,52 @@
+from invoke import task
+from invocations.console import confirm
+
+
+# push
+
+@task
+def test(c):
+	result = c.run("nosetests -v", warn=True)
+	if result.failed and not confirm("Tests failed. Continue?"):
+		print("Aborted at user request.")
+
+@task
+def commit(c):
+	message = input("Enter a git commit message: ")
+	c.run("git add . && git commit -am '{}'".format(message))
+
+@task
+def push(c):
+	c.run("git push origin master")
+
+@task
+def prepare(c):
+	test()
+	commit()
+	push()
+
+
+# deploy
+
+# def pull(c):
+# 	c.run("git pull origin master")
+
+@task
+def heroku(c):
+	c.run("git push heroku master")
+
+@task
+def heroku_test(c):
+	c.run("heroku run nosetests -v")
+
+@task
+def deploy(c):
+	# pull()
+	test()
+	commit()
+	heroku()
+	heroku_test()
+
+@task
+def rollback(c):
+	c.run("heroku rollback")
